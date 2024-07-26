8412a00293fdb0f0a648ae755d26d5d5d79b412d
Ankit Raj <ankitraj90747@gmail.com>
2024-07-26 15:43:35 +0530

    complete signup page functionality

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -3,15 +3,14 @@ import bcrypt from "bcryptjs/dist/bcrypt.js";
 import customError from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
-    const {userid, email, password} = req.body;
+    const {username, email, password} = req.body;
     const hashPassword = bcrypt.hashSync(password, 10);
-    const newUser = new User({userid, email, password : hashPassword});
+    const newUser = new User({userid: username, email, password : hashPassword });
     try {
         await newUser.save();
-        res.status(200).send("signup successful...");
+        res.status(200).json("signup successful...");
     } catch(err) {
         // res.status(500).send(err.message);
         return next(err);
     }
-    res.status(200).send("signUp successful");
 }
--- api/index.js
+++ api/index.js
@@ -3,6 +3,8 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cors from "cors";
+import bodyParser from "body-parser";
 
 dotenv.config();
 const connection = mongoose
@@ -13,14 +15,24 @@ const connection = mongoose
 const app = express();
 
 app.use(express.json());
+app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.get("/",(req,res) => {
+  res.json({"HI from server" : "Ankit"});
+  return res.end();
+})
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || "Error from function";
-  return res.status(statusCode).send(message);
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success : false,
+    statusCode,
+    message,
+  });
 }) 
 
 app.listen(3000, () => console.log("Server listening at 3000"));
