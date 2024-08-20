90c3e3e173dc77a1e9d5b2bd4491f1b44af5ca14
Ankit Raj <ankitraj90747@gmail.com>
2024-08-20 18:05:52 +0530

    update listing page ui with the sahad's listing page ui

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -4,7 +4,7 @@ function CreateListing() {
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
-        Create a listing
+        Create a Listing
       </h1>
       <form className="flex flex-col sm:flex-row gap-4">
         <div className="flex flex-col gap-4 flex-1">
@@ -13,23 +13,24 @@ function CreateListing() {
             placeholder="Name"
             className="border p-3 rounded-lg"
             id="name"
-            maxLength={62}
-            minLength={10}
+            maxLength="62"
+            minLength="10"
             required
           />
           <textarea
-            placeholder="Descritpion"
-            className="border rounded lg p-3"
+            type="text"
+            placeholder="Description"
+            className="border p-3 rounded-lg"
             id="description"
             required
           />
           <input
             type="text"
+            placeholder="Address"
             className="border p-3 rounded-lg"
             id="address"
             required
           />
-
           <div className="flex gap-6 flex-wrap">
             <div className="flex gap-2">
               <input type="checkbox" id="sale" className="w-5" />
@@ -37,7 +38,7 @@ function CreateListing() {
             </div>
             <div className="flex gap-2">
               <input type="checkbox" id="rent" className="w-5" />
-              <span>rent</span>
+              <span>Rent</span>
             </div>
             <div className="flex gap-2">
               <input type="checkbox" id="parking" className="w-5" />
@@ -57,8 +58,19 @@ function CreateListing() {
               <input
                 type="number"
                 id="bedrooms"
-                min={1}
-                max={10}
+                min="1"
+                max="10"
+                required
+                className="p-3 border border-gray-300 rounded-lg"
+              />
+              <p>Beds</p>
+            </div>
+            <div className="flex items-center gap-2">
+              <input
+                type="number"
+                id="bathrooms"
+                min="1"
+                max="10"
                 required
                 className="p-3 border border-gray-300 rounded-lg"
               />
@@ -68,8 +80,8 @@ function CreateListing() {
               <input
                 type="number"
                 id="regularPrice"
-                min={1}
-                max={10}
+                min="1"
+                max="10"
                 required
                 className="p-3 border border-gray-300 rounded-lg"
               />
@@ -82,8 +94,8 @@ function CreateListing() {
               <input
                 type="number"
                 id="discountPrice"
-                min={1}
-                max={10}
+                min="1"
+                max="10"
                 required
                 className="p-3 border border-gray-300 rounded-lg"
               />
@@ -94,7 +106,7 @@ function CreateListing() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col flex-1">
+        <div className="flex flex-col flex-1 gap-4">
           <p className="font-semibold">
             Images:
             <span className="font-normal text-gray-600 ml-2">
@@ -103,20 +115,17 @@ function CreateListing() {
           </p>
           <div className="flex gap-4">
             <input
+              className="p-3 border border-gray-300 rounded w-full"
               type="file"
               id="images"
               accept="image/*"
               multiple
-              className="p-3 border border-gray-300 rounded w-full"
             />
             <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80">
               Upload
             </button>
           </div>
-          <button
-            className="mt-5 p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity:80
-          "
-          >
+          <button className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
             Create Listing
           </button>
         </div>
