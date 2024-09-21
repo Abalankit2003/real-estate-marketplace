4fb62ed9072b9c2bfdae11c5c683c5716425312d
Ankit Raj <ankitraj90747@gmail.com>
2024-09-21 22:49:08 +0530

    debug no. 1

--- api/index.js
+++ api/index.js
@@ -6,7 +6,10 @@ import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 import path from "path";
+
+
 dotenv.config();
+const app = express();
 
 mongoose
   .connect(process.env.MONGO)
@@ -19,15 +22,10 @@ mongoose
 
 const __dirname = path.resolve();
 
-const app = express();
 
 app.use(express.json());
-
 app.use(cookieParser());
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000!");
-});
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
@@ -48,3 +46,7 @@ app.use((err, req, res, next) => {
     message,
   });
 });
+
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000!");
+    });
