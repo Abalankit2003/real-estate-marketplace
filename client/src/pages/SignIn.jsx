e4d354f38aa2d29255596a5361a12177e0b45fd4
Ankit Raj <ankitraj90747@gmail.com>
2024-09-21 23:25:46 +0530

    debug no. 2

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -5,28 +5,25 @@ import {
   signInStart,
   signInSuccess,
   signInFailure,
-} from "../redux/User/userSlice.js";
+} from "../redux/User/userSlice";
 import OAuth from "../components/OAuth";
 
 export default function SignIn() {
-  const [formData, setFormData] = useState({});
-  const { error, loading } = useSelector((state) => state.user);
-
-  const dispatch = useDispatch();
+  const [formData, setFormData] = useState({email : "", password : ""});
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
-
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     });
   };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-    dispatch(signInStart());
     try {
-      const res = await fetch("api/auth/signin", {
+      dispatch(signInStart());
+      const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -34,6 +31,7 @@ export default function SignIn() {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
+      console.log(data);
       if (data.success === false) {
         dispatch(signInFailure(data.message));
         return;
@@ -41,28 +39,28 @@ export default function SignIn() {
       dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
-      dispatch(signInFailure(error)); // Ensure error is a string
+      dispatch(signInFailure(error.message));
     }
   };
-
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="email"
-          placeholder="Email"
+          placeholder="email"
           className="border p-3 rounded-lg"
           id="email"
           onChange={handleChange}
         />
         <input
           type="password"
-          placeholder="Password"
+          placeholder="password"
           className="border p-3 rounded-lg"
           id="password"
           onChange={handleChange}
         />
+
         <button
           disabled={loading}
           className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
@@ -72,16 +70,12 @@ export default function SignIn() {
         <OAuth />
       </form>
       <div className="flex gap-2 mt-5">
-        <p>Don't have an account?</p>
+        <p>Dont have an account?</p>
         <Link to={"/sign-up"}>
-          <span className="text-blue-700">Sign Up</span>
+          <span className="text-blue-700">Sign up</span>
         </Link>
       </div>
-      {error && (
-        <p className="text-red-500 mt-5">
-          {error}
-        </p>
-      )}
+      {error && <p className="text-red-500 mt-5">{error.message}</p>}
     </div>
   );
 }
